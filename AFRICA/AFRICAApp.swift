59ed98bbdae6ac3337e3db07d96ca259c6da4042
Garrett Horn <garrett@sbfmail.com>
2022-08-15 13:48:59 -0400

    Initial Commit

---
+++ AFRICA/AFRICAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFRICAApp.swift
+//  AFRICA
+//
+//  Created by Garrett Horn on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct AFRICAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
